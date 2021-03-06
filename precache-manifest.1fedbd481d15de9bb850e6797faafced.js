b5fd29abc6865e0bdc7f698c0abd36c0814d596c
gh28942 <GerH@example.com>
2021-03-06 16:36:29 +0100

    Updates

---
+++ precache-manifest.1fedbd481d15de9bb850e6797faafced.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ca1e963724c23e124dd4500990312f",
+    "url": "/euromillions-simulator/index.html"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/euromillions-simulator/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "e1351aeddd4d01452483",
+    "url": "/euromillions-simulator/static/css/main.d4244b12.chunk.css"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/euromillions-simulator/static/js/2.effe3d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/euromillions-simulator/static/js/2.effe3d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1351aeddd4d01452483",
+    "url": "/euromillions-simulator/static/js/main.ccda947c.chunk.js"
+  },
+  {
+    "revision": "f5212893f2eeb4b91cb2",
+    "url": "/euromillions-simulator/static/js/runtime-main.a7303bf4.js"
+  },
+  {
+    "revision": "14f57f6562ef5d253e0b53c26bd4b738",
+    "url": "/euromillions-simulator/static/media/stars.14f57f65.jpg"
+  }
+]);
